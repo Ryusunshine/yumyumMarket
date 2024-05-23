2318c5dbb52291150dc47830d2fa879ed516772b
ryusun <ryusunshine@gmail.com>
2024-05-23 11:55:39 +0900

    feat:ryusun - first commit

---
+++ src/main/java/com/example/yumyummarket/YumyumMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.yumyummarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumyumMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumyumMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yumyummarket/YumyumMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yumyummarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumyumMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
